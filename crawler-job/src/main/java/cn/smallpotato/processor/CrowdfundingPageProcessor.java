ef3b708226944e59e12c2ff2c8ac9355be216e77
panjiebin <709511402@qq.com>
2022-07-13 22:15:52 +0800

    crawler job

--- crawler-job/src/main/java/cn/smallpotato/processor/CrowdfundingPageProcessor.java
+++ crawler-job/src/main/java/cn/smallpotato/processor/CrowdfundingPageProcessor.java
@@ -1,6 +1,7 @@
 package cn.smallpotato.processor;
 
 import cn.smallpotato.entity.Crowdfunding;
+import org.apache.commons.lang3.StringUtils;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -39,16 +40,36 @@ public class CrowdfundingPageProcessor implements PageProcessor {
 
     private Crowdfunding parseDetailPage(Page page) {
         Crowdfunding crowdfunding = new Crowdfunding();
-        crowdfunding.setName(page.getHtml().css("div.short-cut > h3 > span", "text").toString());
+        crowdfunding.setName(page.getHtml().css("div.short-cut > h3 > span", "text").toString().trim());
         Selectable center = page.getHtml().css("div.center > div.center-top > div.col1.project-goal.success");
         String goalStr = center.css("span.goal-money", "text").toString();
-        String goalMoney = goalStr.substring(goalStr.indexOf("¥") + 1).replace(",", "");
-        crowdfunding.setGoal(Double.parseDouble(goalMoney));
-        crowdfunding.setPercentage(center.css("span.percent", "text").toString());
-        String people = page.getHtml().css("div.center > div.center-top > div.col3.support-people > h3 > span", "text").toString();
-        crowdfunding.setPeople(Integer.parseInt(people));
-        String completed = page.getHtml().css("div.center > div.center-top > div.col1.project-goal.success > h3 > span", "text").toString();
-        crowdfunding.setCompleted(Double.parseDouble(completed.replace(",", "")));
+        if (StringUtils.isNotBlank(goalStr)) {
+            // 众筹完成
+            String goalMoney = goalStr.substring(goalStr.indexOf("¥") + 1).replace(",", "");
+            crowdfunding.setGoal(Double.parseDouble(goalMoney));
+            crowdfunding.setPercentage(center.css("span.percent", "text").toString());
+            String completed = page.getHtml().css("div.center > div.center-top > div.col1.project-goal.success > h3 > span", "text").toString();
+            crowdfunding.setCompleted(Double.parseDouble(completed.replace(",", "")));
+        } else {
+            Selectable center2 = page.getHtml().css("div.center > div.center-top > div.col1.project-goal.abort.fail");
+            String str2 = center2.css("p.txt.clearfloat > span.goal-money", "text").toString();
+            if (StringUtils.isNotBlank(str2)) {
+                // 众筹终止
+                crowdfunding.setGoal(Double.parseDouble(str2.substring(str2.indexOf("¥") + 1).replace(",", "")));
+                crowdfunding.setPercentage(center2.css("p.txt.clearfloat > span.percent", "text").toString());
+                String completed = center2.css("h3 > span", "text").toString();
+                crowdfunding.setCompleted(Double.parseDouble(completed.replace(",", "")));
+            } else {
+                // 众筹取消
+                Selectable center3 = page.getHtml().css("div.center > div.center-top > div.col1.project-goal.cancel.fail");
+                String str3 = center3.css("p.txt.clearfloat > span.goal-money", "text").toString();
+                crowdfunding.setGoal(Double.parseDouble(str3.substring(str3.indexOf("¥") + 1).replace(",", "")));
+                crowdfunding.setPercentage(center3.css("p.txt.clearfloat > span.percent", "text").toString());
+                String completed = center3.css("h3 > span", "text").toString();
+                crowdfunding.setCompleted(Double.parseDouble(completed.replace(",", "")));
+            }
+        }
+        crowdfunding.setPeople(Integer.parseInt(page.getHtml().css("div.center > div.center-top > div.col3.support-people > h3 > span", "text").toString()));
         crowdfunding.setUpdates(Integer.parseInt(page.getHtml().css("li.pro-gengxin > span", "text").toString()));
         crowdfunding.setComments(Integer.parseInt(page.getHtml().css("span#common_comment_count", "text").toString()));
         crowdfunding.setLikes(Integer.parseInt(page.getHtml().css("div.nav-wrap-inner > ul.nav-right > li.atten > span", "text").toString()));
